7c7fffbff277d674cd9827af11780b5e7f176212
Laura Sirvent Collado <Laura@iMac-de-Laura.local>
2016-05-21 13:12:46 +0200

    configured cocoapods

---
+++ Pods/Target Support Files/Pods-Mami Weather/Pods-Mami Weather-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Mami_Weather : NSObject
+@end
+@implementation PodsDummy_Pods_Mami_Weather
+@end
---
+++ Pods/Target Support Files/Pods-Mami Weather/Pods-Mami Weather-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Mami_WeatherVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Mami_WeatherVersionString[];
+
